d0422f9d11c1905bed1b684c7ca0c03b731f0c8d
Jimmy McCosker <101635312+witherslayer67@users.noreply.github.com>
2025-01-26 16:14:00 -0500

    changed to L3 gearing
    yes this commit was made on a phone

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,7 +30,7 @@ public final class Constants {
         public static final double speedLimitRot = 0.50;
 
         public static final COTSTalonFXSwerveConstants chosenModule =  
-            COTSTalonFXSwerveConstants.SDS.MK4i.KrakenX60(COTSTalonFXSwerveConstants.SDS.MK4i.driveRatios.L2_5);
+            COTSTalonFXSwerveConstants.SDS.MK4i.KrakenX60(COTSTalonFXSwerveConstants.SDS.MK4i.driveRatios.L3);
 
         /* Drivetrain Constants */
         public static final double trackWidth = Units.inchesToMeters(21.75); 
