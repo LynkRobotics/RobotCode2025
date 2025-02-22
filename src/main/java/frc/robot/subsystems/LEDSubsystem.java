34dd65aa54d601bd4e0f1faf00fe545c899b9699
witherslayer67 <witherslayer67@gmail.com>
2025-02-22 17:17:40 -0500

    Refactor LEDSubsystem to use a single CANdle instance based on selection, simplifying configuration and animation handling

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -22,8 +22,8 @@ import frc.lib.util.LoggedCommands;
 
 public class LEDSubsystem extends SubsystemBase {
   /** Creates a new LEDSubsystem. */
-  private CANdle candleLeft = new CANdle(0, "rio");
-  private CANdle candleRight = new CANdle(1, "rio"); //todo make this a constants
+  private final CANdle candle;
+  private final CandleSelection which;
   private static BaseState baseState = BaseState.DISABLED;
   private static TempState tempState = null;
   private static BaseState lastBaseState = null;
@@ -52,6 +52,11 @@ public class LEDSubsystem extends SubsystemBase {
     ERROR
   }
 
+  public enum CandleSelection {
+    LEFT,
+    RIGHT
+  }
+
   public static final class Colors {
     public static final Color off = new LEDSubsystem.Color(0, 0, 0);
     public static final Color red = new LEDSubsystem.Color(255, 0, 0);
@@ -65,19 +70,24 @@ public class LEDSubsystem extends SubsystemBase {
     public static final Color disabled = new LEDSubsystem.Color(200, 0, 0);
   }
 
-  public LEDSubsystem() {
+  public LEDSubsystem(CandleSelection which) {
+    this.which = which;
+    candle = new CANdle(which == CandleSelection.LEFT ? 0 : 1, "rio");
+    applyConfigs();
+
+    // candle.animate(new RainbowAnimation(1,0.4, 94));
+    animate(new FireAnimation(1.0, 0.38, 94, 0.8, 0.2, false, 8));
+    // setBaseState(BaseState.READY);
+  }
+
+  private void applyConfigs() {
     CANdleConfiguration config = new CANdleConfiguration();
     config.brightnessScalar = 1.0;
     config.stripType = LEDStripType.GRB;
     config.v5Enabled = true;
     config.disableWhenLOS = false; //TODO: verify // TODO: true -- why is this triggering?
 
-    candleLeft.configAllSettings(config);
-    candleRight.configAllSettings(config);
-
-    // m_candle.animate(new RainbowAnimation(1,0.4, 94));
-    animate(new FireAnimation(1.0, 0.38, 94, 0.8, 0.2, false, 8));
-    // setBaseState(BaseState.READY);
+    candle.configAllSettings(config);
   }
 
   public static void setBaseState(BaseState newState) {
@@ -89,8 +99,7 @@ public class LEDSubsystem extends SubsystemBase {
   }
 
   public void animate(Animation animation) {
-    candleLeft.animate(animation);
-    candleRight.animate(animation);
+    candle.animate(animation);
   }
   
   public static void clearTempState() {
@@ -99,20 +108,16 @@ public class LEDSubsystem extends SubsystemBase {
   
   public void setColor(Color color) {
     //System.out.printf("Setting color (%d, %d, %d)%n", color.R, color.G, color.B);
-    candleLeft.setLEDs(color.R, color.G, color.B);
-    candleRight.setLEDs(color.R, color.G, color.B);
+    candle.setLEDs(color.R, color.G, color.B);
   }
 
   public void setRainbow(){
-    candleLeft.clearAnimation(0);
-    candleLeft.animate(new RainbowAnimation(0.50, 0.5, 68, false, 8));
-    candleRight.clearAnimation(0);
-    candleRight.animate(new RainbowAnimation(0.50, 0.5, 68, false, 8));
+    candle.clearAnimation(0);
+    candle.animate(new RainbowAnimation(0.50, 0.5, 68, false, 8));
   }
 
   public void clearAnimation(){
-    candleLeft.clearAnimation(0);
-    candleRight.clearAnimation(0);
+    candle.clearAnimation(0);
   }
 
   private Color tempStateColor(TempState state) {
