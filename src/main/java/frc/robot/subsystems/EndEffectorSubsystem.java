0040fb9a9b99c358b8ed9cd5fc27bac1249a3445
Bob Bell <bob@lynkrobotics.org>
2025-02-22 18:39:06 -0500

    Remove warning

--- src/main/java/frc/robot/subsystems/EndEffectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/EndEffectorSubsystem.java
@@ -4,8 +4,6 @@
 
 package frc.robot.subsystems;
 
-import java.util.logging.Logger;
-
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
