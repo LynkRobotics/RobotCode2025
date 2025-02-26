d7f82f237f94224220ef73227b8280b3c6159d0c
Bob Bell <bob@lynkrobotics.org>
2025-02-26 16:17:34 -0500

    Bring back code and fix indenting

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -17,228 +17,230 @@ import frc.robot.Constants;
 import frc.robot.Constants.Elevator.Stop;
 
 public class LEDSubsystem extends SubsystemBase {
-  /** Creates a new LEDSubsystem. */
-  private static CANdleGroup leds;
-  private static BaseState baseState = BaseState.DISABLED;
-  private static TempState tempState = null;
-  private static BaseState lastBaseState = null;
-  private static TempState lastTempState = null;
-  private static double tempStateExpiry = 0.0;
-  private static Timer tempStateTimer = new Timer();
-  private static double blinkInterval = 0.25;
-  private static Timer blinkTimer = new Timer();
-  private static boolean blinkOff = false;
+    /** Creates a new LEDSubsystem. */
+    private static CANdleGroup leds;
+    private static BaseState baseState = BaseState.DISABLED;
+    private static TempState tempState = null;
+    private static BaseState lastBaseState = null;
+    private static TempState lastTempState = null;
+    private static double tempStateExpiry = 0.0;
+    private static Timer tempStateTimer = new Timer();
+    private static double blinkInterval = 0.25;
+    private static Timer blinkTimer = new Timer();
+    private static boolean blinkOff = false;
 
-  public static class Color {
-    private final int R, G, B;
+    public static class Color {
+        private final int R, G, B;
 
-    public Color(int r, int g, int b) {
-      R = r;
-      G = g;
-      B = b;
+        public Color(int r, int g, int b) {
+            R = r;
+            G = g;
+            B = b;
+        }
     }
-  }
 
-  public enum BaseState {
-    DISABLED,
-    READY,
-  }
-
-  public enum TempState {
-    ERROR,
-    WARNING,
-    INTAKING,
-    ALGAE
-  }
-
-  public static final class Colors {
-    public static final Color off = new LEDSubsystem.Color(0, 0, 0);
-    public static final Color red = new LEDSubsystem.Color(255, 0, 0);
-    public static final Color green = new LEDSubsystem.Color(0, 255, 0);
-    public static final Color teal = new LEDSubsystem.Color(44, 162, 165); // i wonder who that could be
-    public static final Color blue = new LEDSubsystem.Color(0, 0, 255);
-    public static final Color cheesyBlue = new LEDSubsystem.Color(0, 112, 255);
-    public static final Color cyan = new LEDSubsystem.Color(0, 255, 255);
-    public static final Color magenta = new LEDSubsystem.Color(255, 0, 255);
-    public static final Color yellow = new LEDSubsystem.Color(255, 255, 0);
-    public static final Color white = new LEDSubsystem.Color(255, 255, 255);
-    public static final Color lynk = new LEDSubsystem.Color(255, 64, 0);
-    public static final Color disabled = new LEDSubsystem.Color(200, 0, 0);
-  }
-
-  public LEDSubsystem() {
-    leds = new CANdleGroup(
-        new CANdle(Constants.LEDs.leftCandle, Constants.LEDs.canBus),
-        new CANdle(Constants.LEDs.rightCandle, Constants.LEDs.canBus));
-
-    leds.clearAnimation();
-    leds.animate(Constants.LEDs.fireAnimation);
-  }
-
-  public static void setBaseState(BaseState newState) {
-    baseState = newState;
-  }
-
-  public static void setTempState(TempState newState) {
-    tempState = newState;
-  }
-
-  public static void clearTempState() {
-    tempState = null;
-  }
-
-  public static void setColor(Color color) {
-    leds.setLEDs(color.R, color.G, color.B);
-  }
-
-  public static void setRainbow() {
-    leds.clearAnimation();
-    leds.animate(Constants.LEDs.rainbowAnimation);
-  }
-
-  public static void setLEDs(Color color, int count) {
-    leds.clearAnimation();
-    leds.setLEDs(color.R, color.G, color.B, 255, Constants.LEDs.startIdx, count);
-  }
-
-  public static void setLEDs(Color color) {
-    leds.clearAnimation();
-    leds.setLEDs(0, 0, 0);
-  }
-
-  public static void setLarson(Color color, int count) {
-    leds.clearAnimation();
-    leds.animate(new LarsonAnimation(color.R, color.G, color.B, 255, 0.5, count, LarsonAnimation.BounceMode.Front, 7, 8));
-  }
-
-  private static Color tempStateColor(TempState state) {
-    if (state == TempState.ERROR) {
-      return Colors.red;
+    public enum BaseState {
+        DISABLED,
+        READY,
     }
-    if (state == TempState.WARNING) {
-      return Colors.yellow;
-    }
-    if (state == TempState.INTAKING) {
-      return Colors.white;
-    }
-    if (state == TempState.ALGAE) {
-      return Colors.teal;
-    } else {
-      DogLog.log("LED/Status", "tempStateColor: Unknown state: " + state);
-      return Colors.off;
-    }
-  }
 
-  private static Color baseStateColor(BaseState state) {
-    if (state == BaseState.DISABLED) {
-      return Colors.disabled;
-    } else if (state == BaseState.READY) {
-      return Colors.lynk;
-    } else {
-      DogLog.log("LED/Status", "baseStateColor: Unknown state: " + state);
-      return Colors.off;
+    public enum TempState {
+        ERROR,
+        WARNING,
+        INTAKING,
+        ALGAE
     }
-  }
 
-  @Override
-  public void periodic() {
-    SmartDashboard.putString("LED/Base state", baseState == null ? "NULL" : baseState.toString());
-    SmartDashboard.putString("LED/Temp state", tempState == null ? "NULL" : tempState.toString());
+    public static final class Colors {
+        public static final Color off = new LEDSubsystem.Color(0, 0, 0);
+        public static final Color red = new LEDSubsystem.Color(255, 0, 0);
+        public static final Color green = new LEDSubsystem.Color(0, 255, 0);
+        public static final Color teal = new LEDSubsystem.Color(44, 162, 165); // i wonder who that could be
+        public static final Color blue = new LEDSubsystem.Color(0, 0, 255);
+        public static final Color cheesyBlue = new LEDSubsystem.Color(0, 112, 255);
+        public static final Color cyan = new LEDSubsystem.Color(0, 255, 255);
+        public static final Color magenta = new LEDSubsystem.Color(255, 0, 255);
+        public static final Color yellow = new LEDSubsystem.Color(255, 255, 0);
+        public static final Color white = new LEDSubsystem.Color(255, 255, 255);
+        public static final Color lynk = new LEDSubsystem.Color(255, 64, 0);
+        public static final Color disabled = new LEDSubsystem.Color(200, 0, 0);
+    }
 
-    /*
-     * if (enabled){
-     * if (LoggedAlert.ERROR == True){
-     * Flash.RED
-     * }
-     * if (LoggedAlert.WARNING == True){
-     * Flash.YELLOW
-     * }
-     * if (isIntaking){
-     * Flash.WHITE
-     * }
-     * if (goingForAlgae){
-     * Flash.ElevatorLevel.HighTideTeal
-     * }
-     * if (movingElevatorForScoringCoral){
-     * DripUp.ElevatorLevel.WHITE
-     * DeadLEDs.BLACK
-     * }
-     * if (hasCoral.inIndex){
-     * Solid.WHITE
-     * }
-     * if (){
-     * }
-     * }
-     */
-    // if (DriverStation.isEnabled()) {
-      // If the temporary state is active...
-      // if (tempState != null) {
-        // Clear current animation
-        // clearAnimation();
-        // if (tempState == lastTempState) {
-          // Temporary state unchanged
-          // if (tempStateExpiry > 0.0 && tempStateTimer.hasElapsed(tempStateExpiry)) {
-            // Temporary state has expired, and base state should be shown
-            // tempState = null;
-          // } else {
-            // Temporary state is active, but might need to be blinked
-            // if (blinkTimer.hasElapsed(blinkInterval)) {
-              // blinkOff = !blinkOff;
-              // if (blinkOff) {
-                // setColor(Colors.off);
-              // } else {
-                // setColor(tempStateColor(tempState));
-              // }
-              // blinkTimer.restart();
-            // }
-          // }
-        // } else {
-          // Start new temporary state
-          // setColor(tempStateColor(tempState));
-          // blinkOff = false;
-          // blinkTimer.restart();
-          // if (tempState == TempState.ERROR || tempState == TempState.WARNING) {
-            // blinkInterval = 0.10;
-            // tempStateExpiry = 0.80;
-            // tempStateTimer.restart();
-          // } else {
-            // blinkInterval = 0.20;
-            // tempStateExpiry = 0.0;
-          // }
-        // }
-      // }
-// 
-      // if (RobotState.getNextStop().equals(Stop.L1)) {
-        // setBaseState(null);
-        // setLEDs(Colors.white, 24);
-      // } else if (RobotState.getNextStop().equals(Stop.L2)) {
-        // setBaseState(null);
-        // setLEDs(Colors.white, 48);
-      // } else if (RobotState.getNextStop().equals(Stop.L3)) {
-        // setBaseState(null);
-        // setLEDs(Colors.white, 72);
-      // } else if (RobotState.getNextStop().equals(Stop.L4) || RobotState.getNextStop().equals(Stop.L4_SCORE)) {
-        // setBaseState(null);
-        // setLEDs(Colors.white, 94);
-      // } else {
-        // setBaseState(BaseState.READY);
-      // }
-// 
-      // if (tempState == null) { // Check for a changed base state, or a dropped temporary state
-        // Check for possible temporary startup condition, and skip it
-        // if (baseState == null) {
-          // DogLog.log("LED/Status", "LEDSubsystem::periodic: Base State NULL");
-        // } else {
-          // if (baseState != lastBaseState || lastTempState != null) {
-            // setColor(baseStateColor(baseState));
-            // lastBaseState = baseState;
-          // }
-        // }
-      // }
-// 
-      // Update the last states processed for reference in the next iteration
-      // lastTempState = tempState;
-      // lastBaseState = baseState;
-    // }
-  }
+    public LEDSubsystem() {
+        leds = new CANdleGroup(
+                new CANdle(Constants.LEDs.leftCandle, Constants.LEDs.canBus),
+                new CANdle(Constants.LEDs.rightCandle, Constants.LEDs.canBus));
+
+        leds.clearAnimation();
+        leds.animate(Constants.LEDs.fireAnimation);
+    }
+
+    public static void setBaseState(BaseState newState) {
+        baseState = newState;
+    }
+
+    public static void setTempState(TempState newState) {
+        tempState = newState;
+    }
+
+    public static void clearTempState() {
+        tempState = null;
+    }
+
+    public static void setColor(Color color) {
+        leds.setLEDs(color.R, color.G, color.B);
+    }
+
+    public static void setRainbow() {
+        leds.clearAnimation();
+        leds.animate(Constants.LEDs.rainbowAnimation);
+    }
+
+    public static void setLEDs(Color color, int count) {
+        leds.clearAnimation();
+        leds.setLEDs(color.R, color.G, color.B, 255, Constants.LEDs.startIdx, count);
+    }
+
+    public static void setLEDs(Color color) {
+        leds.clearAnimation();
+        leds.setLEDs(0, 0, 0);
+    }
+
+    public static void setLarson(Color color, int count) {
+        leds.clearAnimation();
+        leds.animate(new LarsonAnimation(color.R, color.G, color.B, 255, 0.5, count, LarsonAnimation.BounceMode.Front,
+                7, 8));
+    }
+
+    private static Color tempStateColor(TempState state) {
+        if (state == TempState.ERROR) {
+            return Colors.red;
+        }
+        if (state == TempState.WARNING) {
+            return Colors.yellow;
+        }
+        if (state == TempState.INTAKING) {
+            return Colors.white;
+        }
+        if (state == TempState.ALGAE) {
+            return Colors.teal;
+        } else {
+            DogLog.log("LED/Status", "tempStateColor: Unknown state: " + state);
+            return Colors.off;
+        }
+    }
+
+    private static Color baseStateColor(BaseState state) {
+        if (state == BaseState.DISABLED) {
+            return Colors.disabled;
+        } else if (state == BaseState.READY) {
+            return Colors.lynk;
+        } else {
+            DogLog.log("LED/Status", "baseStateColor: Unknown state: " + state);
+            return Colors.off;
+        }
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putString("LED/Base state", baseState == null ? "NULL" : baseState.toString());
+        SmartDashboard.putString("LED/Temp state", tempState == null ? "NULL" : tempState.toString());
+
+        /*
+         * if (enabled){
+         * if (LoggedAlert.ERROR == True){
+         * Flash.RED
+         * }
+         * if (LoggedAlert.WARNING == True){
+         * Flash.YELLOW
+         * }
+         * if (isIntaking){
+         * Flash.WHITE
+         * }
+         * if (goingForAlgae){
+         * Flash.ElevatorLevel.HighTideTeal
+         * }
+         * if (movingElevatorForScoringCoral){
+         * DripUp.ElevatorLevel.WHITE
+         * DeadLEDs.BLACK
+         * }
+         * if (hasCoral.inIndex){
+         * Solid.WHITE
+         * }
+         * if (){
+         * }
+         * }
+         */
+
+        if (DriverStation.isEnabled()) {
+            // If the temporary state is active...
+            if (tempState != null) {
+                // Clear current animation
+                leds.clearAnimation();
+                if (tempState == lastTempState) {
+                    // Temporary state unchanged
+                    if (tempStateExpiry > 0.0 && tempStateTimer.hasElapsed(tempStateExpiry)) {
+                        // Temporary state has expired, and base state should be shown
+                        tempState = null;
+                    } else {
+                        // Temporary state is active, but might need to be blinked
+                        if (blinkTimer.hasElapsed(blinkInterval)) {
+                            blinkOff = !blinkOff;
+                            if (blinkOff) {
+                                setColor(Colors.off);
+                            } else {
+                                setColor(tempStateColor(tempState));
+                            }
+                            blinkTimer.restart();
+                        }
+                    }
+                } else {
+                    // Start new temporary state
+                    setColor(tempStateColor(tempState));
+                    blinkOff = false;
+                    blinkTimer.restart();
+                    if (tempState == TempState.ERROR || tempState == TempState.WARNING) {
+                        blinkInterval = 0.10;
+                        tempStateExpiry = 0.80;
+                        tempStateTimer.restart();
+                    } else {
+                        blinkInterval = 0.20;
+                        tempStateExpiry = 0.0;
+                    }
+                }
+            }
+
+            if (RobotState.getNextStop().equals(Stop.L1)) {
+                setBaseState(null);
+                setLEDs(Colors.white, 24);
+            } else if (RobotState.getNextStop().equals(Stop.L2)) {
+                setBaseState(null);
+                setLEDs(Colors.white, 48);
+            } else if (RobotState.getNextStop().equals(Stop.L3)) {
+                setBaseState(null);
+                setLEDs(Colors.white, 72);
+            } else if (RobotState.getNextStop().equals(Stop.L4) || RobotState.getNextStop().equals(Stop.L4_SCORE)) {
+                setBaseState(null);
+                setLEDs(Colors.white, 94);
+            } else {
+                setBaseState(BaseState.READY);
+            }
+
+            if (tempState == null) { // Check for a changed base state, or a dropped temporary state
+                // Check for possible temporary startup condition, and skip it
+                if (baseState == null) {
+                    DogLog.log("LED/Status", "LEDSubsystem::periodic: Base State NULL");
+                } else {
+                    if (baseState != lastBaseState || lastTempState != null) {
+                        setColor(baseStateColor(baseState));
+                        lastBaseState = baseState;
+                    }
+                }
+            }
+
+            // Update the last states processed for reference in the next iteration
+            lastTempState = tempState;
+            lastBaseState = baseState;
+        }
+    }
 }
