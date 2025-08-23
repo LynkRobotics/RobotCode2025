4ab28a589a6ad45921ce64d3da62686b64d98d17
Bob Bell <bob@lynkrobotics.org>
2025-08-23 12:23:05 -0400

    Slow mode was REALLY slow

--- src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
@@ -15,7 +15,7 @@ import frc.robot.Ports;
 
 public class SwerveConstants {
     public static final String swerveCanBus = Ports.Bus.SWERVE.name;
-    public static final double slowMode = 0.3;
+    public static final double slowMode = 0.5;
     
     public static final COTSTalonFXSwerveConstants chosenModule = COTSTalonFXSwerveConstants.WCP.SwerveX2t.KrakenX60(COTSTalonFXSwerveConstants.WCP.SwerveX2t.driveRatios.X3_10);
 
