c524f5c9101e0ca8acfb87f89a2be92923b3042b
Bob Bell <bob@lynkrobotics.org>
2025-09-02 18:42:33 -0400

    Allow for less annoying climber reset during practice

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -51,7 +51,7 @@ public class Climber extends SubsystemBase {
         intakeMotor = new TalonFX(Ports.CLIMBER_ROLLERS.id, Ports.CLIMBER_ROLLERS.bus.name);
         intakeMotor.getConfigurator().apply(ClimberConstants.getIntakeMotorConfig());
 
-        deployMotor.setPosition(ClimberPosition.STOWED.angle);
+        deployMotor.setPosition(ClimberConstants.startsClear ? ClimberPosition.CLEAR.angle : ClimberPosition.STOWED.angle);
         deployMotor.setControl(ClimberPosition.CLEAR.control);
 
         SmartDashboard.putData("Climber/Start Reset", StartReset());
